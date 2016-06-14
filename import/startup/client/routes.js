9d9751d7ed38fc3566c97c5489af6a9f77c4e862
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-06-14 14:11:17 -0500

    Added prototype file structure

--- import/startup/client/routes.js
+++ import/startup/client/routes.js
@@ -5,3 +5,31 @@ import { AccountsTemplates } from 'meteor/useraccounts:core';
 // Import to load templates including layouts and pages
 
 // Define routes
+FlowRouter.route('/', {
+  name: 'App.home',
+  action() {
+    BlazeLayout.render('App_body', { main: 'Users_page' });
+  }
+});
+
+FlowRouter.route('/login', {
+  name: 'App.login',
+  action() {
+    BlazeLayout.render('Login_body');
+  },
+});
+
+FlowRouter.route('/edit', {
+  name: 'App.edit',
+  action() {
+    BlazeLayout.render('Edit_body');
+  },
+});
+
+// TODO Add user and study groups routes
+
+// Configure account routess
+AccountsTemplates.configureRoute('signIn', {
+  name: 'signin',
+  path: '/signin',
+});
---
+++ import/ui/layouts/app-body.js
@@ -0,0 +1,23 @@
+import './app-body.html';
+
+import { Meteor } from 'meteor/meteor';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { Lists } from '../../api/lists/lists.js';
+import { Template } from 'meteor/templating';
+import { ActiveRoute } from 'meteor/zimme:active-route';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { TAPi18n } from 'meteor/tap:i18n';
+
+Template.App_body.onCreated(function appBodyOnCreated() {
+  if (!Meteor.user()) {
+    FlowRouter.go('App.login');
+  }
+
+  // Add subscriptions
+
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    // Add default state values
+  });
+});
