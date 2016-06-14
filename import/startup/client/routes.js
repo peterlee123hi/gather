b5623b27b34ea7544c2f6bfacfe262bafe59fbc1
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-06-14 12:34:04 -0500

    Added base files

---
+++ import/startup/client/routes.js
@@ -0,0 +1,7 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { AccountsTemplates } from 'meteor/useraccounts:core';
+
+// Import to load templates including layouts and pages
+
+// Define routes
---
+++ import/startup/server/security.js
@@ -0,0 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+import { DDPRateLimiter } from 'meteor/ddp-rate-limiter';
+import { _ } from 'meteor/underscore';
+
+// Don't let people write arbitrary data to their 'profile' field from the client
+Meteor.users.deny({
+  update() {
+    return true;
+  },
+});
+
+if (Meteor.isServer) {
+  // Only allow 2 login attempts per connection per 5 seconds
+  DDPRateLimiter.addRule({
+    name(name) {
+      return _.contains(AUTH_METHODS, name);
+    },
+
+    // Rate limit per connection ID
+    connectionId() { return true; },
+  }, 2, 5000);
+}
