28b742b4b724a38af6b074b01fdd16d4fca381e9
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-06-15 16:27:48 -0500

    Default template is officially bug free

--- imports/startup/client/useraccounts-configuration.js
+++ imports/startup/client/useraccounts-configuration.js
@@ -2,10 +2,6 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { AccountsTemplates } from 'meteor/useraccounts:core';
 
 AccountsTemplates.configure({
-  defaultTemplate: 'Login_page',
-  defaultLayout: 'Login_body',
-  defaultContentRegion: 'main',
-  defaultLayoutRegions: {},
   onLogoutHook: () => {
     FlowRouter.go('login');
   }
