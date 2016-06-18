6c75f72f29670dda12c7a15de7520e92dd5a7a4c
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-06-18 00:57:52 -0500

    Connected pages

--- imports/ui/pages/app-page.js
+++ imports/ui/pages/app-page.js
@@ -4,5 +4,13 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { _ } from 'meteor/underscore';
 
 import '../components/users-view.js';
+import '../components/study-groups-view.js';
+import '../components/create-view.js';
 
 import './app-page.html';
+
+Template.App_page.helpers({
+  isPage(page) {
+    return Session.get('page') === page;
+  }
+});
